b64e4d74458ef756f6cc7912a3b5c620e75e5874
Shoan Chikato <sjchikato@gmail.com>
2022-01-02 15:30:03 +0200

    return detailed role object with permissions populated

--- src/repo/role.repo.js
+++ src/repo/role.repo.js
@@ -3,7 +3,8 @@ const { InternalServerError } = require("../error/errors");
 function roleRepoFactory(db) {
   const save = async (role) => {
     try {
-      return db.save(role);
+      const newRole = await db.save(role);
+      return await db.findOne(newRole.id, { relations: ["permissions"] });
     } catch (error) {
       throw new InternalServerError(
         `error occurred saving role \n\n${error.stack}`
